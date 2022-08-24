5f320f614ebe0a70d82a2f55fd240ba325c213f4
muhammad-jiku <muhammadjiku364@gmail.com>
2022-08-24 14:54:39 +0600

    configuration has been added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import { VideoSDKMeeting } from '@videosdk.live/rtc-js-prebuilt';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+  useEffect(() => {
+    const apiKey = process.env.REACT_APP_VIDEOSDK_API_KEY;
+    const meetingId = 'milkyway';
+    const name = 'Demo User';
+
+    const config = {
+      name: name,
+      meetingId: meetingId,
+      apiKey: apiKey,
+
+      containerId: null,
+
+      micEnabled: true,
+      webcamEnabled: true,
+      participantCanToggleSelfWebcam: true,
+      participantCanToggleSelfMic: true,
+      raiseHandEnabled: true,
+      chatEnabled: true,
+      screenShareEnabled: true,
+      whiteboardEnabled: true,
+
+      //   debug
+      debug: true,
+
+      //   leftScreen
+      leftScreen: {
+        // actionButton: {
+        //   label: 'Video SDK',
+        //   href: 'https://videosdk.live/',
+        // },
+        rejoinButtonEnabled: true,
+      },
+
+      //  join screen
+      joinScreen: {
+        visible: true,
+        title: 'Your Meeting',
+        meetingUrl: 'customURL.com',
+      },
+
+      //   layout
+      layout: {
+        type: 'SIDEBAR', // "SPOTLIGHT" | "SIDEBAR" | "GRID"
+        priority: 'PIN', // "SPEAKER" | "PIN",
+        gridSize: 3,
+      },
+
+      //   recording
+      recording: {
+        enabled: true,
+        webhookUrl: 'https://www.videosdk.live/callback',
+        //   awsDirPath: `/meeting-recordings/${meetingId}/`,
+        autoStart: false,
+      },
+
+      //   live stream
+      livestream: {
+        autoStart: true,
+        enabled: true,
+      },
+
+      //   permissions
+      permissions: {
+        pin: true,
+        toggleRecording: true,
+        toggleLivestream: true,
+        changeLayout: true,
+        // askToJoin: true,
+        toggleParticipantWebcam: true,
+        toggleParticipantMic: true,
+        toggleParticipantScreenshare: true,
+        removeParticipant: true,
+        endMeeting: true,
+        canCreatePoll: true,
+        drawOnWhiteboard: true,
+        toggleWhiteboard: true,
+      },
+    };
+
+    const meeting = new VideoSDKMeeting();
+    meeting.init(config);
+  }, []);
+
+  return <div></div>;
 }
-
-export default App;
